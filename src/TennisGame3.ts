23b6e690aa7f82777c512fe24aaa7bdd9e09a968
hmln <41984636+hmln@users.noreply.github.com>
2020-11-30 17:39:35 +0700

    fix: refactor for TennisGame3

--- src/TennisGame3.ts
+++ src/TennisGame3.ts
@@ -13,17 +13,21 @@ export class TennisGame3 implements TennisGame {
   }
 
   getScore(): string {
-    let s: string;
-    if (this.p1 < 4 && this.p2 < 4 && !(this.p1 + this.p2 === 6)) {
-      const p: string[] = ['Love', 'Fifteen', 'Thirty', 'Forty'];
-      s = p[this.p1];
-      return (this.p1 === this.p2) ? s + '-All' : s + '-' + p[this.p2];
-    } else {
-      if (this.p1 === this.p2)
+    const maxPoint = Math.max(this.p1, this.p2)
+    
+    if (this.p1 === this.p2 && this.p1 >= 3) {
         return 'Deuce';
-      s = this.p1 > this.p2 ? this.p1N : this.p2N;
-      return (((this.p1 - this.p2) * (this.p1 - this.p2)) === 1) ? 'Advantage ' + s : 'Win for ' + s;
     }
+    
+    if (maxPoint >= 4) {
+      const playerHasMorePoint = this.p1 > this.p2 ? this.p1N : this.p2N;
+      const sloganPrefix =  Math.abs(this.p1 - this.p2) === 1 ? 'Advantage' : 'Win for';
+      return sloganPrefix + ' ' + playerHasMorePoint; 
+    }
+    
+    const p: string[] = ['Love', 'Fifteen', 'Thirty', 'Forty'];
+    const s1 = p[this.p1], s2 = p[this.p2];
+    return (s1 === s2) ? s1 + '-All' : s1 + '-' + s2;
   }
 
   wonPoint(playerName: string): void {

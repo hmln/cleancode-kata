4f6a6b712e1f5ebf5f2b5f69267c2f12fc20bf5d
hmln <41984636+hmln@users.noreply.github.com>
2020-11-30 21:21:31 +0700

    fix: refactor TennisGame2

--- src/TennisGame2.ts
+++ src/TennisGame2.ts
@@ -33,43 +33,12 @@ export class TennisGame2 implements TennisGame {
     }
   }
 
-  whenP2EqualZero(): void {
-    this.P1res = pointToName(this.P1point)
-    this.P2res = 'Love';
-    this.scoreSlogan = this.P1res + '-' + this.P2res;
-  }
-
-  whenP1EqualZero(): void {
-    this.P2res = pointToName(this.P2point)
-    this.P1res = 'Love';
-    this.scoreSlogan = this.P1res + '-' + this.P2res;
-  }
-
   onePlayerLargerThanTheOther(): void {
     this.P1res = pointToName(this.P1point)
     this.P2res = pointToName(this.P2point)
     this.scoreSlogan = this.P1res + '-' + this.P2res;
   }
-  whenPlayer1NearlyWin(): void {
-    if (this.P2point >= 3) {
-      this.scoreSlogan = 'Advantage player1';
-    }
-  }
-  whenPlayer2NearlyWin(): void {
-    if (this.P1point >= 3) {
-      this.scoreSlogan = 'Advantage player2';
-    }
-  }
-  whenEitherPlayerEqualZero(): void {
-    if (this.P1point > 0) {
-      this.whenP2EqualZero()
-    }
-    if (this.P2point > 0) {
-      this.whenP1EqualZero()
-    }
-  }
-  
-  whenEitherPlayerWon(distanceScore): void {
+  findTheWinner(distanceScore): void {
     if (distanceScore >= 2) {
       this.scoreSlogan = 'Win for player1';
     }
@@ -77,33 +46,35 @@ export class TennisGame2 implements TennisGame {
       this.scoreSlogan = 'Win for player2';
     }
   }
+  findTheAdvantagePlayer(distanceScore): void {
+    if (distanceScore == 1){
+      this.scoreSlogan = 'Advantage player1';
+    }
+    if (distanceScore == -1) {
+      this.scoreSlogan = 'Advantage player2';
+    }
+  }
   getScore(): string {
     const distanceScore: number = this.P1point - this.P2point;
     const maxScore: number = Math.max(this.P1point, this.P2point)
     const minScore: number = Math.min(this.P1point, this.P2point)
+    // When both players' scores are equal
     if (this.P1point === this.P2point) {
       this.whenBothLessThanThreshold()
     }
-    if (minScore === 0){
-      this.whenEitherPlayerEqualZero()
-    }
-    
-    if (distanceScore > 0){
-      if (this.P1point < this.THRESHOLD) {
-        this.onePlayerLargerThanTheOther();
+    // Players' scores are different
+    else {
+      if (maxScore >= this.THRESHOLD) {
+        if (Math.abs(distanceScore) >= 2) {
+          this.findTheWinner(distanceScore);
+        }
+        else {
+          this.findTheAdvantagePlayer(distanceScore)
+        }
       }
-      this.whenPlayer1NearlyWin();
-    }
-    
-    if (distanceScore < 0) {
-      if (this.P2point < this.THRESHOLD) {
-        this.onePlayerLargerThanTheOther();
+      else {
+        this.onePlayerLargerThanTheOther()
       }
-      this.whenPlayer2NearlyWin();
-    }
-    
-    if (maxScore >= this.THRESHOLD) {
-      this.whenEitherPlayerWon(distanceScore)
     }
     return this.scoreSlogan;
   }
